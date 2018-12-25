94e930174635d22a5f4ea6c933109539289bbe61
Sergii <sergiy.stotskiy@gmail.com>
2018-12-25 12:11:22 +0200

    refactor(example): uses typescript
    Refactors to typescript to show the validity of types for @casl/* packages: https://github.com/stalniy/casl/issues/136

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # casl-mongoose-example
-CASL and mongoose example
+
+CASL and mongoose example on TypeScript
+
+## Build
+
+```sh
+# build source to dist folder
+npm run build
+```
+
+## Run in dev mode
+
+```sh
+# uses ts-node to run index.ts
+npm run dev
+```
--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
-const mongoose = require('mongoose')
-const { AbilityBuilder } = require('@casl/ability')
-const { accessibleRecordsPlugin, permittedFieldsPlugin } = require('@casl/mongoose')
-const { pick } = require('lodash')
+import * as mongoose from 'mongoose'
+import { AbilityBuilder } from '@casl/ability'
+import { accessibleRecordsPlugin, permittedFieldsPlugin } from '@casl/mongoose'
+import { pick } from 'lodash'
 
 mongoose.plugin(accessibleRecordsPlugin)
 mongoose.plugin(permittedFieldsPlugin)
@@ -11,7 +11,7 @@ const ability = AbilityBuilder.define((can) => {
   can('read', 'Post', ['id', 'description', 'title'], { public: false })
 })
 
-const PostSchema = mongoose.Schema({
+const PostSchema = new mongoose.Schema({
   title: String,
   description: String,
   public: { type: Boolean, default: false }
@@ -21,8 +21,9 @@ const Post = mongoose.model('Post', PostSchema)
 
 async function main() {
   await mongoose.connect('mongodb://localhost/casl_test')
+  const amountOfPosts = await Post.count({})
 
-  if (!(await Post.count())) {
+  if (!amountOfPosts) {
     await Promise.all([
       Post.create({
         title: 'My post',
@@ -37,18 +38,19 @@ async function main() {
   }
 
   const query = Post.accessibleBy(ability)
-  const posts = await query.exec()
-  const postsUsingSelect = await query.select(Post.permittedFieldsBy(ability).join(' '))
+  const posts = await query.find()
+  const postsUsingSelect = await query.select(Post.accessibleFieldsBy(ability).join(' '))
 
   return {
     // because posts with `public: true` should not contain `description` field
     incorrect: postsUsingSelect,
 
     // the next results are correct
-    correct: posts.map(post => pick(post, post.permittedFieldsBy(ability))),
+    correct: posts.map(post => pick(post, post.accessibleFieldsBy(ability))),
   }
 }
 
 main()
   .then(console.log)
   .catch(console.error)
+  .then(() => mongoose.connection.close())
