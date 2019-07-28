44b4563b5f502c1ccf9662930923f5aed4f2ed6f
Sergii <sergiy.stotskiy@gmail.com>
2019-07-28 10:21:00 +0300

    feat(example): adds example with discriminator
    Relates to stalniy/casl#212

--- index.ts
+++ index.ts
@@ -1,53 +1,55 @@
 import * as mongoose from 'mongoose'
 import { AbilityBuilder } from '@casl/ability'
-import { accessibleRecordsPlugin, permittedFieldsPlugin } from '@casl/mongoose'
-import { pick } from 'lodash'
+import { accessibleRecordsPlugin } from '@casl/mongoose'
 
 mongoose.plugin(accessibleRecordsPlugin)
-mongoose.plugin(permittedFieldsPlugin)
+
+mongoose.set('debug', true)
 
 const ability = AbilityBuilder.define((can) => {
-  can('read', 'Post', ['id', 'title'], { public: true })
-  can('read', 'Post', ['id', 'description', 'title'], { public: false })
+  can('read', 'Page', { public: true })
+  can('read', 'BlogPost')
 })
 
-const PostSchema = new mongoose.Schema({
+const PageSchema = new mongoose.Schema({
   title: String,
   description: String,
   public: { type: Boolean, default: false }
+}, {
+  discriminatorKey: 'type'
 })
 
-const Post = mongoose.model('Post', PostSchema)
+const Page = mongoose.model('Page', PageSchema)
+const BlogPost = Page.discriminator('BlogPost', new mongoose.Schema({
+  comments: [String],
+}))
 
 async function main() {
   await mongoose.connect('mongodb://localhost/casl_test')
-  const amountOfPosts = await Post.count({})
+  const amountOfPosts = await Page.count({})
 
   if (!amountOfPosts) {
     await Promise.all([
-      Post.create({
-        title: 'My post',
+      Page.create({
+        title: 'About',
         description: ':)',
         public: true
       }),
-      Post.create({
-        title: 'Not my post',
+      Page.create({
+        title: 'Home',
         description: 'description'
+      }),
+      BlogPost.create({
+        title: 'My blog post',
+        comments: ['comment 1', 'comment 2'],
+        public: true
       })
     ])
   }
 
-  const query = Post.accessibleBy(ability)
-  const posts = await query.find()
-  const postsUsingSelect = await query.select(Post.accessibleFieldsBy(ability).join(' '))
+  const pages = await BlogPost.accessibleBy(ability)
 
-  return {
-    // because posts with `public: true` should not contain `description` field
-    incorrect: postsUsingSelect,
-
-    // the next results are correct
-    correct: posts.map(post => pick(post, post.accessibleFieldsBy(ability))),
-  }
+  return pages
 }
 
 main()
